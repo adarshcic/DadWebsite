826bb8bbc25c632dc666f7bc0adac397a4d54ffd
adarshcic <lekhprakash.bhagawan-cic-uk@ibm.com>
2017-09-28 15:29:34 +0100

    added smooth scroll
    it works on google chrome now as well
    made the code look tidier

---
+++ navbar-cont.js
@@ -0,0 +1,42 @@
+document.write('\
+<div class="nav-wrapper-cont">\
+<div id="mynav"class="nav navbar-fixed" style="background-color: #fff; margin-bottom:-100px; padding-top:10px; padding-left:150px;padding-right:150px;" data-spy="affix" data-offset-top="110">\
+  <div class="row row-no-padding" style="margin-bottom:-50px">\
+    <div class="col-xs-5" style="margin-bottom:-60px">\
+      <a href="index.html" style="padding-left:20px;"><img src="./Images/translogo.png" alt="BMCL" width="140" height="100"></a>\
+      <p id="myP">A 360&deg; business review</p>\
+    </div>\
+  </div>\
+\
+\
+  <div class="row row-no-padding" style="margin-top:-80px; margin-bottom:-50px">\
+      <div align="right">\
+      <nav class="navbar navbar-default" style="background-color:#ffffff;">\
+        <div class="container-fluid">\
+\
+          <ul class="nav navbar-nav navbar-right" style="margin-bottom:-50px;background-color:#fff">\
+            <li class="highlight"><a href="#about" >About</a></li>\
+            <li class="highlight"><a href="#approach">Approach</a></li>\
+            <li class="highlight"><a href="#services">Services</a></li>\
+            <li class="highlight"><a href="#projects">Projects</a></li>\
+            <li class="highlight"><a href="#clients">Clients</a></li>\
+            <li class="highlight"><a href="#contact">More Info</a></li>\
+            <li class="highlight"> <a href="#"> <img src="./Images/thickicon.png" alt="Search" width="24px" height="24px" onclick="unhide()"> </a> </li>\
+          </ul>\
+        </div>\
+      </nav>\
+      </div>\
+    </div>\
+\
+    <div class="row">\
+      <div class="col-xs-12" style="margin-top:-20px;">\
+      <div id="open" align="right" style="padding-right:20px; display:none;">\
+      <form>\
+        <input type="search" name="search" placeholder="Search.."></input>\
+      </form>\
+      </div>\
+    </div>\
+    </div>\
+</div>\
+</div>\
+')
